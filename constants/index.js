898316ff29093ae4aa820034018df4505a1b3472
mazeboyy <136639590+mazeboyy@users.noreply.github.com>
2023-07-31 21:51:49 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x22ffd84b88dcc945bb61b340fda2c38544ec3a80";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
